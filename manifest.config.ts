b138842a8657752f2d97f4cab94eb4aea54e1e71
Hai Le <hai.le@elmosoftware.com.au>
2026-03-25 14:18:09 +1100

    chore: update package.json and manifest.config.ts

--- manifest.config.ts
+++ manifest.config.ts
@@ -3,7 +3,8 @@ import pkg from './package.json'
 
 export default defineManifest({
   manifest_version: 3,
-  name: pkg.name,
+  name: 'Bitbucket: Format commit message',
+  description: 'Format the commit message for Bitbucket pull requests',
   version: pkg.version,
   icons: {
     48: 'public/logo.png',
